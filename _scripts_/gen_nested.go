7bd192651a2bc1ec7137dbf0052d14916c5969ed
mewmew <rnd0x00@gmail.com>
2017-09-04 15:18:06 +0200

    _scripts_: add preliminary version of gen_nested

---
+++ _scripts_/gen_nested.go
@@ -0,0 +1,51 @@
+// gen_nested is a tool which generates C programs with a given number of nested
+// if-statements. These programs are intended to stress test the
+// implementations of various decompilation components and give an approximation
+// of their time complexity.
+//
+// Example output for n=2:
+//
+//    int main(int argc, char **argv) {
+//    	int x = 0;
+//    	if (x < 1) {
+//    		x++;
+//    		if (x < 2) {
+//    			x++;
+//    		}
+//    	}
+//    	return x%113;
+//    }
+package main
+
+import (
+	"flag"
+	"fmt"
+	"strings"
+)
+
+const (
+	pre = `
+int main(int argc, char **argv) {
+	int x = 0;`
+	post = `
+	return x%123;
+}`
+)
+
+func main() {
+	var n int
+	flag.IntVar(&n, "n", 0, "Number of nested if-statements.")
+	flag.Parse()
+
+	fmt.Println(pre[1:])
+	for i := 0; i < n; i++ {
+		indent := strings.Repeat("\t", i)
+		fmt.Printf("%s\tif (x < %d) {\n", indent, i+1)
+		fmt.Printf("%s\t\tx++;\n", indent)
+	}
+	for i := n - 1; i >= 0; i-- {
+		indent := strings.Repeat("\t", i)
+		fmt.Printf("%s\t}\n", indent)
+	}
+	fmt.Println(post[1:])
+}
