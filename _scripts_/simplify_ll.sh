9b9302eead83730c53eb0dc2286301df4e1ce2b4
mewmew <rnd0x00@gmail.com>
2018-02-10 00:01:31 +0100

    _scripts_: add simplify_ll.sh script
    Note, this scripts makes use of the regular expression search
    and replace tool sar. To install, run:
    
        go get github.com/mewkiz/cmd/sar

---
+++ _scripts_/simplify_ll.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# This script is a hack, converting LLVM IR assembly files produced by Clang
+# into the format expected by ll2dot and ll2go. Most notably the metadata nodes
+# are stripped.
+
+# Parse command line arguments.
+if [ $# -ne 1 ]; then
+	echo "Usage: simplify_ll.sh FILE.ll"
+	exit 1
+fi
+ll_file=$1
+
+sar -i ', ![a-z.]+ ![0-9]+' '' ${ll_file}
+sar -i ' !dbg ![0-9]+ {[\n]' ' {\n' ${ll_file}
+sar -i '  tail call void @llvm.dbg.value[^\n]+[\n]' '' ${ll_file}
+sar -i '  call void @llvm.dbg.value[^\n]+[\n]' '' ${ll_file}
+sar -i '  tail call void @llvm.dbg.declare[^\n]+[\n]' '' ${ll_file}
+sar -i '[\n]!(.|[\n])*' '' ${ll_file}
